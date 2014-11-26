75f38fdf93eac1b09e41f0f3aa4a8b4398558950
EmilyPlatzer <emilyplatzer@gmail.com>
2014-11-26 11:37:07 -0700

    Posts and post are served up in JSON

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'pg'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
+gem 'rails-api', '~> 0.3.1'
 # gem 'bcrypt', '~> 3.1.7'
 
 group :test, :development do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,17 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
+    render json: @posts
   end
 
   def show
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def edit
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @post.update(post_params)
-      redirect_to @post, notice: 'Post was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+    render json: @post
   end
 
   private
     def set_post
       @post = Post.find(params[:id])
-    end
-
-    def post_params
-      params.require(:post).permit(:title, :author, :date, :excerpt, :body)
     end
 end
