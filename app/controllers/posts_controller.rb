6bf3e7452d4848ad9697edce5175463f6a43e768
EmilyPlatzer <emilyplatzer@gmail.com>
2014-11-27 07:48:06 -0700

    Posts are being served via JSON with posts as key

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,8 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all.order(:date)
-    render json: @posts
+    render json: {posts: @posts}
   end
 
-  def show
-    render json: @post
-  end
-
-  private
-    def set_post
-      @post = Post.find(params[:id])
-    end
 end
